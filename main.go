0ec8f3f89e13ebed63ea03dd1f7f85288b95f556
wirapratamaz <wira@gringgo.co>
2023-04-03 11:43:28 +0800

    url parsing

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+func main() {
+	var urlString = "http://developer.com:80/hello?name=airell&age=23"
+	var u, e = url.Parse(urlString)
+
+	if e != nil {
+		fmt.Println(e.Error())
+		return
+	}
+
+	fmt.Printf("url: %s\n", urlString)
+
+	fmt.Printf("protocol: %s\n", u.Scheme)
+	fmt.Printf("host: %s\n", u.Host)
+	fmt.Printf("path: %s\n", u.Path)
+
+	var name = u.Query()["name"][0]
+	var age = u.Query()["age"][0]
+	fmt.Printf("nameL %s, age: %s\n", name, age)
+}
